0b190d3839f8c75264628a35b92725cc18a7f0c7
KingJab30 <jabirabdullah10@gmail.com>
2024-09-13 15:08:43 +0000

    3rd_task executed and completed

---
+++ beginner/task_3.py
@@ -0,0 +1,8 @@
+def numCheck():
+    num = float(input("Enter the number: "))
+    if num%2 ==0:
+        print("its an even number!!")
+    else:
+        print("its an odd number!!")
+    return 0
+numCheck()
